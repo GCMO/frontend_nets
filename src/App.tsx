c94e4b1866c6cc7706b030a13a6b0017eb33abf1
GCMO <g.c.m.orlandi@gmail.com>
2023-01-20 16:56:16 +0100

    refactored App.tsx and to-do-card.tsx, next will try to add DragNDrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useCallback } from "react";
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+// import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 // import  update  from 'immutability-helper';
-
 
 import getData from "./api/getData";
 import { Button } from "./components/button";
@@ -14,7 +13,7 @@ interface Todo {
   status: string;
 }
 
-const App: React.FC = () => {
+const App: React.FC<Todo> = (todo) => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -23,7 +22,6 @@ const App: React.FC = () => {
     try {
       const response = await getData();
       setTodoList(response);
-      console.log('DATA', response)
     } catch (error) {
       console.error(error);
     } finally {
@@ -31,14 +29,16 @@ const App: React.FC = () => {
     }
   }, []);
 
-  const handleDragEnd = (result: any) => {
-    if (!result.destination) return;
-    const items = Array.from(todoList);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+  // const handleDragEnd = (result: DropResult) => {
+  //   const { source, destination } = result
+  //   if (!result.destination) return;
 
-    setTodoList(items);
-  };
+  //   const items = Array.from(todoList);
+  //   const [reorderedItem] = items.splice(result.source.index, 1);
+  //   items.splice(result.destination.index, 0, reorderedItem);
+
+  //   setTodoList(items);
+  // };
 
   return (
     <div className="App w-screen h-screen justify-center items-center m-12">
@@ -51,79 +51,92 @@ const App: React.FC = () => {
               <img src="/loader.png" alt="Loading..." />
               <p>Loading...</p>
             </div>
-        ) : todoList.length ? ( 
-          <div className="container">
-            <div className="flex flex-row gap-3 bg-slate-200 rounded-lg">
-              <DragDropContext onDragEnd={handleDragEnd}>
-                <Droppable droppableId={'todoList'}>
-                  {(provided) => (
-                  <>
-                    <div className="flex-col w-1/2 text-xl p-4" {...provided.droppableProps} ref={provided.innerRef}> <b>PENDING </b>
-                    { todoList
-                      .filter((todo) => (todo.status === "pending")) 
-                      .map((todo, index) => (
-                        <Draggable key={todo.id} draggableId={`todoList`} index={index}>
-                          {(provided) => (
-                          <div
-                           ref={provided.innerRef}
-                           {...provided.draggableProps}
-                           {...provided.dragHandleProps}
-                          >   
-                            <ToDoCard 
-                              key={todo.id} 
-                              title={todo.title} 
-                              id={todo.id} 
-                              due_on={todo.due_on} 
-                              status={todo.status}  
-                            /> 
-                            </div>
-                          )}
-                        </Draggable>
-                      ))}
-                      {provided.placeholder}
-                    </div>
+          ) : todoList.length ? (          
+            <ToDoCard 
+                id={todo.id}
+                title={todo.title}
+                due_on={todo.due_on}
+                status={todo.status}
+            /> 
+          ) : (
+            <div className="text-xl m-14  ">
+                <p><b>Nothing to see here...</b></p>
+                <p>Grab something to do</p>
+                <Button onClick={handleClick}/>
+            </div> 
+          )
+        }
 
-                    <div className="flex-col w-1/2 text-xl p-4" {...provided.droppableProps} ref={provided.innerRef}><b>COMPLETED</b>
-                    { todoList
-                      .filter((todo) => (todo.status === "completed"))
-                      .map((todo, index) => (
-                        <Draggable key={todo.id} draggableId={`todoList`} index={index}>
-                          {(provided) => (  
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                          >                          
-                            <ToDoCard 
-                              key={todo.id} 
-                              title={todo.title} 
-                              id={todo.id} 
-                              due_on={todo.due_on} 
-                              status={todo.status} 
-                            />
-                          </div>
-                          )}
-                        </Draggable>
-                      ))}
-                      {provided.placeholder}
-                    </div>
-                  </>
-                  )}
-              </Droppable>
-            </DragDropContext>
-            </div>
-              <Button onClick={handleClick}/>
-          </div>
-        ) : (
-          <div className="text-xl m-14  ">
-              <p><b>Nothing to see here...</b></p>
-              <p>Grab something to do</p>
-              <Button onClick={handleClick}/>
-          </div> 
-        )}  
     </div>
   );
 };
 
 
 export default App;
+
+          // {/* <div className="container">
+          //    <div className="flex flex-row gap-3 bg-slate-200 rounded-lg">
+          //      <DragDropContext onDragEnd={handleDragEnd}>
+          //       <Droppable droppableId={'todoList'}>
+          //         {(provided) => ( */}
+          //         <>
+          //           {/* <div className='todoList' ref={provided.innerRef} {...provided.droppableProps}>
+          //           <div className="flex-col w-1/2 text-xl p-4" {...provided.droppableProps} ref={provided.innerRef}> <b>PENDING </b>
+          //           { todoList
+          //             .filter((todo) => (todo.status === "pending")) 
+          //             .map((todo, index) => ( 
+          //               {/* <Draggable key={todo.id} draggableId={`${todo.id}-${todo.status}`} index={index}>
+          //                 {(provided, shapshot) => (
+          //                 <div
+          //                  ref={provided.innerRef}
+          //                  {...provided.draggableProps}
+          //                  {...provided.dragHandleProps}
+          //                 >    */}
+          //                   {/* <ToDoCard 
+          //                     key={todo.id} 
+          //                     title={todo.title} 
+          //                     id={todo.id} 
+          //                     due_on={todo.due_on} 
+          //                     status={todo.status}  
+          //                   />  */}
+          //                   {/* </div>
+          //                 )}
+          //               </Draggable>
+          //             ))}
+          //             {provided.placeholder}
+          //           </div> */}
+
+          //           {/* <div className="flex-col w-1/2 text-xl p-4" {...provided.droppableProps} ref={provided.innerRef}><b>COMPLETED</b>
+          //            { todoList
+          //             .filter((todo) => (todo.status === "completed"))
+          //             .map((todo, index) => (
+          //               <Draggable key={todo.id} draggableId={`${todo.id}-${todo.status}`} index={index}>
+          //                 {(provided) => (  
+          //                 <div
+          //                   ref={provided.innerRef}
+          //                   {...provided.draggableProps}
+          //                   {...provided.dragHandleProps}
+          //                 >                           */}
+          //                   {/* <ToDoCard 
+          //                     key={todo.id} 
+          //                     title={todo.title} 
+          //                     id={todo.id} 
+          //                     due_on={todo.due_on} 
+          //                     status={todo.status} 
+          //                   /> */}
+          //                 {/* </div> */}
+          //                 {/* )}
+          //               </Draggable>
+          //             ))}
+          //             {provided.placeholder}
+          //           </div>
+          //           </div>
+          //         </>
+          //         )}
+          //     </Droppable>
+          //   </DragDropContext> */}
+          //     {/* <Button onClick={handleClick}/>
+          //   </div>
+          // </div> */}
+
+        
--- src/components/to-do-card.tsx
+++ src/components/to-do-card.tsx
@@ -1,3 +1,8 @@
+import {useState, useCallback} from 'react';
+
+import {Button} from './button';
+import getData from '../api/getData';
+
 interface CardProps {
   id: number;
   title: string;
@@ -8,23 +13,69 @@ interface CardProps {
 
 export const ToDoCard: React.FC<CardProps> = ({ id, title, due_on, status }) => {
 
+  const [todoList, setTodoList] = useState<CardProps[]>([]);
+
+  const handleClick = useCallback (async () => {
+    try {
+      const response = await getData();
+      setTodoList(response);
+      console.log('DATA', response)
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+  
+  
   return (
-    <div className={` ${status === "completed" ? "bg-green-500" : "bg-orange-300"} relative text-lg drop-shadow-lg rounded-lg px-3 py-6 my-2`}>
-      <div>
-        <div className="relative mr-5 mb-2 ">{title}</div>
-        <div className="absolute h-4 w-9 text-xs bottom-2 right-3">#{id}</div>
-        <div className="absolute h-4 w-fit text-xs bottom-2 left-3">Due: {due_on}</div>
-      </div>
-    </div>
+    
+    <div className="container">
+      <div className="flex flex-row gap-3 bg-slate-200 rounded-lg">
+          
+              <div className="flex-col w-1/2 text-xl p-4"> <b>PENDING </b>
+              { todoList
+                .filter((todo) => ( todo.status === "pending" )) 
+                .map((todo, index) => ( 
+                  
+                    <div key={index} className={` ${todo.status === "completed" ? "bg-green-500" : "bg-orange-300"} relative text-lg drop-shadow-lg rounded-lg px-3 py-6 my-2`}>
+                      <div>
+                        <div className="relative mr-5 mb-2 ">{todo.title}</div>
+                        <div className="absolute h-4 w-9 text-xs bottom-2 right-3">#{todo.id}</div>
+                        <div className="absolute h-4 w-fit text-xs bottom-2 left-3">Due: {todo.due_on}</div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+
+              <div className="flex-col w-1/2 text-xl p-4"><b>COMPLETED</b>
+              { todoList
+                .filter((todo) => (todo.status === "completed"))
+                .map((todo, index) => (                 
+                  <div key={index} className={` ${todo.status === "completed" ? "bg-green-500" : "bg-orange-300"} relative text-lg drop-shadow-lg rounded-lg px-3 py-6 my-2`}>
+                    <div >
+                      <div className="relative mr-5 mb-2 ">{todo.title}</div>
+                      <div className="absolute h-4 w-9 text-xs bottom-2 right-3">#{todo.id}</div>
+                      <div className="absolute h-4 w-fit text-xs bottom-2 left-3">Due: {todo.due_on}</div>
+                    </div>
+                  </div>
+                  ))}
+                </div>
+              </div>
+           <Button onClick={handleClick}/>
+      
+     </div>
+  
   )
-}
+};
 
 
 
-
-
-
-
-
-
-
+//ORIGINAL   return (
+//     <div className={` ${status === "completed" ? "bg-green-500" : "bg-orange-300"} relative text-lg drop-shadow-lg rounded-lg px-3 py-6 my-2`}>
+//       <div>
+//         <div className="relative mr-5 mb-2 ">{title}</div>
+//         <div className="absolute h-4 w-9 text-xs bottom-2 right-3">#{id}</div>
+//         <div className="absolute h-4 w-fit text-xs bottom-2 left-3">Due: {due_on}</div>
+//       </div>
+//     </div>
+//   )
+// }
